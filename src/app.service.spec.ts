d805395ba8d2e0be22715ee6ff7ff62081380f6f
yukifury <forlorn128@gmail.com>
2026-06-02 00:18:38 +0100

    chore(app): remove starter hello service

--- src/app.service.spec.ts
+++
@@ -1,9 +0,0 @@
-import { AppService } from './app.service';
-
-describe('AppService', () => {
-  it('should return the default greeting', () => {
-    const appService = new AppService();
-
-    expect(appService.getHello()).toBe('Hello World!');
-  });
-});
